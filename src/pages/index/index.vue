714daeeb266378f1b4ecc46a38ebc287d47d5aec
ZQR7 <2397721424@qq.com>
2019-11-27 02:06:59 +0800

    new

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -20,6 +20,16 @@
         <i-grid-label>{{grid.title}}</i-grid-label>
     </i-grid-item>
   </i-grid>
+
+   <i-grid i-class="no-border">
+    <i-grid-item @click="goType(krid)" v-for="krid in krids" :key="krid" i-class="no-border">
+        <i-grid-icon>
+            <image :src="krid.image" />
+        </i-grid-icon>
+        <i-grid-label>{{krid.title}}</i-grid-label>
+    </i-grid-item>
+  </i-grid>
+
   <i-panel :title="title_name">
     <view style="padding: 15px;">
       <i-card v-for="item in top" :key="item" i-class="split" :extra="item.name" :thumb="item.image">
@@ -35,19 +45,21 @@
 export default {
   data () {
     return {
-      title_name: "热门学习内容",
+
       grids: [
         {title:"定语",image:"/static/images/1.png"},
         {title:"定语从句",image:"/static/images/2.png"},
         {title:"倒装句",image:"/static/images/3.png"},
         {title:"强调句",image:"/static/images/4.png"}
       ],
-      top: [
-        {name:"店名1",address:"地址1",image:"https://i.loli.net/2017/08/21/599a521472424.jpg",remark:"介绍1"},
-        {name:"店名2",address:"地址2",image:"https://i.loli.net/2017/08/21/599a521472424.jpg",remark:"介绍2"},
-        {name:"店名3",address:"地址3",image:"https://i.loli.net/2017/08/21/599a521472424.jpg",remark:"介绍3"},
-        {name:"店名4",address:"地址4",image:"https://i.loli.net/2017/08/21/599a521472424.jpg",remark:"介绍4"}
+
+      krids: [
+        {title:"状语",image:"/static/images/5.png"},
+        {title:"主语从句",image:"/static/images/6.png"},
+        {title:"动词",image:"/static/images/7.png"},
+        {title:"宾语从句",image:"/static/images/8.png"}
       ],
+
       imgUrls: [
         'https://images.unsplash.com/photo-1551334787-21e6bd3ab135?w=640',
         'https://images.unsplash.com/photo-1551214012-84f95e060dee?w=640',
