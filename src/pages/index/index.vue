860d3c46fb53df645aa1a70ac612d4c8cbe27d82
ZQR7 <2397721424@qq.com>
2019-11-26 18:05:52 +0800

    new

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,22 +1,22 @@
 <template>
   <div>
     <i-grid i-class="no-border">
-    <i-grid-item v-for="grid in grids" :key="grid" i-class="no-border">
+    <i-grid-item @click="goType(grid)" v-for="grid in grids" :key="grid" i-class="no-border">
         <i-grid-icon>
             <image :src="grid.image" />
         </i-grid-icon>
         <i-grid-label>{{grid.title}}</i-grid-label>
     </i-grid-item> 
-    </i-grid>
+   </i-grid>
 
-      <i-panel :title="title_name">
-          <view style="padding: 15px:">
-             <i-card i-class="split" title="卡片标题" extra="额外内容" thumb="https://i.loli.net/2017/08/21/599a521472424.jpg">
-                 <view slot="content">内容不错</view>
-                 <view slot="footer">尾部内容</view>
-             </i-card>
-          </view>
-      </i-panel>   
+    <i-panel :title="title_name">
+        <view style="padding: 15px;">
+          <i-card v-for="item in top" :key="item" i-class="split" :extra="item.name" :thumb="item.image">
+              <view slot="content">{{item.remark}}</view>
+              <view slot="footer">{{item.address}}</view>
+          </i-card>
+        </view>
+    </i-panel>   
 
   </div>
 </template>
@@ -25,20 +25,26 @@
 export default {
   data () {
     return {
-        title_name:"热门",
-        girds:[
-            {title:"定语",image:"/static/images/user.png"},
-            {title:"定语从句",image:"/static/images/user.png"},
-            {title:"倒装句",image:"/static/images/user.png"},
-            {title:"强调句",image:"/static/images/user.png"}
+        title_name: "热门学习内容",
+        girds: [
+          {title:"定语",image:"/static/images/1.png"},
+          {title:"定语从句",image:"/static/images/2.png"},
+          {title:"倒装句",image:"/static/images/3.png"},
+          {title:"强调句",image:"/static/images/4.png"}
         ],
         top:[
-            {name:"名字",address:"",image}
+            {name:"名字1",address:"地址1",image:"https://i.loli.net/2017/08/21/599a521472424.jpg",remark:"介绍1"},
+            {name:"名字2",address:"地址2",image:"https://i.loli.net/2017/08/21/599a521472424.jpg",remark:"介绍2"},
+            {name:"名字3",address:"地址3",image:"https://i.loli.net/2017/08/21/599a521472424.jpg",remark:"介绍3"},
+            {name:"名字4",address:"地址4",image:"https://i.loli.net/2017/08/21/599a521472424.jpg",remark:"介绍4"}
         ]
     }
   },
 
   methods: { 
+    goType(type){
+      console.log(type)
+    }
    },
 
   created () {
